559eed1f48e4aa534833ce6878a97ec0e7f6062f
Combatd <combatdstar@gmail.com>
2021-02-06 05:29:21 -0800

    Write while, do while, for loops

--- BranchesAndLoops/BranchesAndLoops/Program.cs
+++ BranchesAndLoops/BranchesAndLoops/Program.cs
@@ -67,6 +67,44 @@ namespace BranchesAndLoops
         {
 
             //ExploreIf();
+
+            //int counter = 0;
+            //while (counter < 10)
+            //{
+            //    Console.WriteLine($"Hello World! The counter is {counter}");
+            //    counter++;
+            //}
+
+            /*
+             * Make sure that the while loop condition changes to false as you execute the code. 
+             * Otherwise, you create an infinite loop where your program never ends.
+             */
+
+            // The while loop tests the condition before executing the code following the while.
+            // The do while loop executes the code first, and then checks the condition.
+
+            int counter = 0;
+            do
+            {
+                Console.WriteLine($"Hello World! The counter is {counter}");
+                counter++;
+            } while (counter < 10);
+
+            // initializer; condition; iterator
+            for (int index = 0; index < 10; index++)
+            {
+                Console.WriteLine($"Hello World! The index is {index}");
+            }
+
+            // O(n^2)
+            for (int row = 1; row < 11; row++)
+            {
+                for (char column = 'a'; column < 'k'; column++)
+                {
+                    Console.WriteLine($"The cell is ({row}, {column})");
+                }
+            }
+
         }
     }
 }
