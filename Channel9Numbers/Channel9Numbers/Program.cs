5e128669a04a4879b607c1a600d289ec429681ea
Combatd <combatdstar@gmail.com>
2021-02-06 01:00:34 -0800

    Define Program#WorkingWithIntegers, Program#OrderPrecedence

--- Channel9Numbers/Channel9Numbers/Program.cs
+++ Channel9Numbers/Channel9Numbers/Program.cs
@@ -4,26 +4,28 @@ namespace Channel9Numbers
 {
     class Program
     {
-        static void Main(string[] args)
+        static void WorkingWithIntegers()
         {
            // You remember Integers in school, let's see how Math and Integers behave in C#!
            // Spoiler alert - it's more intuitive than you'd expect! We'll talk about order of operations,
            // how numbers divide cleanly(or don't!) and lots more.
 
 
-            int a = 18;
-            int b = 6;
-            // addition - 24
-            int c = a + b;
-            Console.WriteLine(c);
+            //int a = 18;
+            //int b = 6;
+            //// addition - 24
+            //int c = a + b;
+            //Console.WriteLine(c);
 
-            // subtraction - 12
-            c = a - b;
-            Console.WriteLine(c);
+            //// subtraction - 12
+            //c = a - b;
+            //Console.WriteLine(c);
 
-            // division - 3
-            c = a / b;
-            Console.WriteLine(c);
+            //// division - 3
+            //c = a / b;
+            //Console.WriteLine(c);
+        }
+        static void OrderPrecedence() {
 
             /*
              * 
@@ -35,7 +37,30 @@ namespace Channel9Numbers
              *
              */
 
-   
+            int a = 5;
+            int b = 4;
+            int c = 2;
+            int d = a + b * c;
+            Console.WriteLine(d); // 13
+
+            // The output demonstrates that the multiplication is performed before the addition.
+            /*
+             * You can force a different order of operation by adding parentheses 
+             * around the operation or operations you want performed first. 
+             * Add the following lines and run again:
+             */
+
+            d = (a + b) * c;
+            Console.WriteLine(d); // 16
+
+            d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+            Console.WriteLine(d); // 25
         }
+
+        static void Main(string[] args) {
+            WorkingWithIntegers();
+            OrderPrecedence();
+        }
+
     }
 }
