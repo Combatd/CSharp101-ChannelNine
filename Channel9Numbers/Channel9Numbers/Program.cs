482ca2e201695b53a7c6fe58f4fae26ddf6674b0
Combatd <combatdstar@gmail.com>
2021-02-06 01:09:17 -0800

    Hit limits of C# integer overflow and underflow

--- Channel9Numbers/Channel9Numbers/Program.cs
+++ Channel9Numbers/Channel9Numbers/Program.cs
@@ -60,6 +60,33 @@ namespace Channel9Numbers
         static void Main(string[] args) {
             WorkingWithIntegers();
             OrderPrecedence();
+
+            int a = 7;
+            int b = 4;
+            int c = 3;
+            int d = (a + b) / c;
+            int e = (a + b) % c;
+            Console.WriteLine($"quotient: {d}");
+            Console.WriteLine($"remainder: {e}");
+
+            // If a calculation produces a value that exceeds those limits, you have an underflow or overflow condition.
+            // The answer appears to wrap from one limit to the other. 
+            int max = int.MaxValue;
+            int min = int.MinValue;
+            Console.WriteLine($"The range of integers is {min} to {max}");
+
+            int what = max + 3;
+            Console.WriteLine($"An example of overflow: {what}");
+
+            /*
+             * Notice that the answer is very close to the minimum (negative) integer. It's the same as min + 2. The addition operation overflowed the allowed values for integers. The answer is a very large negative number because an overflow "wraps around" from the largest possible integer value to the smallest.
+
+                There are other numeric types with different limits and precision that you would use when the int type doesn't meet your needs. Let's explore those other types next.
+
+                Once again, let's move the code you wrote in this section into a separate method. Name it TestLimits.
+             * 
+             * 
+             */
         }
 
     }
