2757584c29751a6e313e34faba77905b7919a07b
Combatd <combatdstar@gmail.com>
2021-02-08 08:39:37 -0800

    Print out all elements in List<string> names

--- list_tutorial/list_tutorial/Program.cs
+++ list_tutorial/list_tutorial/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace list_tutorial
 {
@@ -6,7 +7,11 @@ namespace list_tutorial
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var names = new List<string> { "<name>", "Ana", "Felipe" };
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
         }
     }
 }
