1ab8d5117de54bbcfd90df5cb8b5be25a0c19359
Combatd <combatdstar@gmail.com>
2021-02-09 02:54:24 -0800

    Override PerformMonthEndTransactions in class LineOfCreditAmount

--- classes/classes/LineOfCreditAccount.cs
+++ classes/classes/LineOfCreditAccount.cs
@@ -8,10 +8,21 @@ namespace classes
         Will incur a fee on each withdrawal that goes over the credit limit.
      */
 
-    public class LineOfCreditAccount
+    public class LineOfCreditAccount : BankAccount
     {
-        public LineOfCreditAccount()
+        public LineOfCreditAccount(string name, decimal initialBalance) : base(name, initialBalance)
         {
         }
+
+        public override void PerformMonthEndTransactions()
+        {
+            if (Balance < 0)
+            {
+                // Negate the balance to get a positive interest charge:
+                var interest = -Balance * 0.07m;
+                MakeWithdrawal(interest, DateTime.Now, "Charge monthly interest");
+            }
+        }
+
     }
 }
