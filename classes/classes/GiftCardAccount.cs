1e9365f939d57932da1c6287649c64a38e023462
Combatd <combatdstar@gmail.com>
2021-02-09 02:57:32 -0800

    Override virtual PerformMonthEndTransactions in class GiftCardAccount

--- classes/classes/GiftCardAccount.cs
+++ classes/classes/GiftCardAccount.cs
@@ -6,10 +6,24 @@ namespace classes
         Can be refilled with a specified amount once each month, on the last day of the month.
      */
 
-    public class GiftCardAccount
+    public class GiftCardAccount : BankAccount
     {
-        public GiftCardAccount()
+        private decimal _monthlyDeposit = 0m;
+
+        public GiftCardAccount(string name, decimal initialBalance, decimal monthlyDeposit = 0) : base(name, initialBalance)
+            => _monthlyDeposit = monthlyDeposit;
+
+
+        /*
+         * The constructor provides a default value for the monthlyDeposit value so callers can omit a 0 for no monthly deposit.
+         */
+
+        public override void PerformMonthEndTransactions()
         {
+            if (_monthlyDeposit != 0)
+            {
+                MakeDeposit(_monthlyDeposit, DateTime.Now, "Add monthly deposit");
+            }
         }
     }
 }
