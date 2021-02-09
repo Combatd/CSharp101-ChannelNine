1ca00bc9443eda5e7871222099a7b4d7e7f8c183
Combatd <combatdstar@gmail.com>
2021-02-08 21:57:41 -0800

    Declare List<Int> fibonacciNumbers and add 18 new elements

--- list_tutorial/list_tutorial/Program.cs
+++ list_tutorial/list_tutorial/Program.cs
@@ -8,6 +8,21 @@ namespace list_tutorial
         static void Main(string[] args)
         {
             WorkingWithStrings();
+
+            var fibonacciNumbers = new List<int> { 1, 1 };
+
+            // Try to write the code to generate the first 20 numbers in the sequence. (As a hint, the 20th Fibonacci number is 6765.)
+            // first 2 numbers already in
+            for (int i = 2; i < 20; i++)
+            {
+                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+                fibonacciNumbers.Add(previous + previous2);
+            }
+
+            foreach (var item in fibonacciNumbers)
+                Console.WriteLine(item);
         }
 
         static void WorkingWithStrings()
