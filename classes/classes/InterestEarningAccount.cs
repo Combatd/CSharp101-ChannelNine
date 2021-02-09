e8bb3c4f8e7d7a05e42bdb00ee38dc9d0a997dd9
Combatd <combatdstar@gmail.com>
2021-02-09 02:51:52 -0800

    Override virtual PerformMonthEndTransactions in class InterestEarningAccount

--- classes/classes/InterestEarningAccount.cs
+++ classes/classes/InterestEarningAccount.cs
@@ -17,5 +17,14 @@ namespace classes
 
         }
 
+        public override void PerformMonthEndTransactions()
+        {
+            if (Balance > 500m)
+            {
+                var interest = Balance * 0.05m;
+                MakeDeposit(interest, DateTime.Now, "apply monthly interest");
+            }
+        }
+
     }
 }
