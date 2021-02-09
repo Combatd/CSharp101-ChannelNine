dec009de9e31299f3f95af916553e0a3cf05de2a
Combatd <combatdstar@gmail.com>
2021-02-08 23:02:59 -0800

    Define BankAccount constructor

--- classes/classes/BankAccount.cs
+++ classes/classes/BankAccount.cs
@@ -8,6 +8,13 @@ namespace classes
         public string Owner { get; set; }
         public decimal Balance { get; }
 
+        // constructor method
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Balance = initialBalance;
+        }
+
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
         }
