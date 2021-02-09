de8cf70119142f8674660d83e854680a2cc8322a
Combatd <combatdstar@gmail.com>
2021-02-08 21:51:38 -0800

    Search and Sort names list

--- list_tutorial/list_tutorial/Program.cs
+++ list_tutorial/list_tutorial/Program.cs
@@ -26,6 +26,35 @@ namespace list_tutorial
             Console.WriteLine($"I've added {names[2]} and {names[3]} to the list");
 
             Console.WriteLine($"The list has {names.Count} people in it");
+
+
+            var index = names.IndexOf("Felipe");
+            if (index == -1)
+            {
+                Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+            }
+            else
+            {
+                Console.WriteLine($"The name {names[index]} is at index {index}");
+            }
+
+            index = names.IndexOf("Not Found");
+            if (index == -1)
+            {
+                Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+            }
+            else
+            {
+                Console.WriteLine($"The name {names[index]} is at index {index}");
+
+            }
+
+            names.Sort();
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
         }
     }
 }
