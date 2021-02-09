f2f2e7677982126044ac05299c84fcd3f17cff7a
Combatd <combatdstar@gmail.com>
2021-02-08 21:21:39 -0800

    Log string interpolation to terminal and print names.Count

--- list_tutorial/list_tutorial/Program.cs
+++ list_tutorial/list_tutorial/Program.cs
@@ -21,6 +21,11 @@ namespace list_tutorial
             {
                 Console.WriteLine($"Hello {name.ToUpper()}!");
             }
+
+            Console.WriteLine($"My name is {names[0]}");
+            Console.WriteLine($"I've added {names[2]} and {names[3]} to the list");
+
+            Console.WriteLine($"The list has {names.Count} people in it");
         }
     }
 }
